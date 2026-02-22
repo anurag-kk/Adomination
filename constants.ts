74e974f654cb9460d80c21b8c14e5d636163c581
anurag-kk <anurag.chess5@gmail.com>
2026-02-22 21:40:11 +0530

    added hong kong and puerto rico

--- constants.ts
+++ constants.ts
@@ -9,7 +9,7 @@ export const COUNTRIES = [
   "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Eswatini", "Ethiopia",
   "Fiji", "Finland", "France",
   "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Greece", "Grenada", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana",
-  "Haiti", "Honduras", "Hungary",
+  "Haiti", "Honduras", "Hungary","Hong Kong", "Puerto Rico",
   "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Israel", "Italy", "Ivory Coast",
   "Jamaica", "Japan", "Jordan",
   "Kazakhstan", "Kenya", "Kiribati", "Kosovo", "Kuwait", "Kyrgyzstan",
